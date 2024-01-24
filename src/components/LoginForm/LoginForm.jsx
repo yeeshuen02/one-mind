16e9d8cdae62d371988ee06a244adb189a227892
Yee Shuen <yeeshuen3@Angs-MacBook-Pro-2.local>
2024-01-24 17:11:37 +0800

    updated login page with empty hooks

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./LoginForm.css";
 
 const LoginForm = () => {
+  const emailRef = useRef();
+  const errRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [email, pwd]);
+
+  function handleSubmit() {}
+
   return (
     <div className="wrapper">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Welcome</h1>
         <hr className="line" />
 
         <div className="input-box">
-          <input type="text" placeholder="Clinician ID" required />
+          <input
+            type="text"
+            placeholder="Clinician Email"
+            ref={emailRef}
+            autoComplete="off"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
         </div>
+
         <div className="input-box">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            autoComplete="off"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+          />
         </div>
 
         <div className="forgot">
