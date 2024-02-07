06f15ca0f95034159ca131438385963499dc012f
Nicholas Yong Weng Hong <nicholasyong03@gmail.com>
2024-02-08 00:39:22 +0800

    Your commit message here

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCphBN6FVBdSzxmabhIjVLWmn0-pZi9xh0",
@@ -17,4 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const database = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/scenes/Homepage/Homepage.jsx
+++ src/scenes/Homepage/Homepage.jsx
@@ -1,7 +1,319 @@
-import React from "react";
+import React, { useState } from 'react';
+
+import homePageOneMindLogo from "../../assets/logo-blue.png"
+import searchIcon from "../../assets/search-icon.png"
+import ageIcon from "../../assets/filter-age-icon.png"
+import dateIcon from "../../assets/calendar-icon.png"
+import genderIcon from "../../assets/gender-icon.png"
+import phq9Icon from "../../assets/PHQ9-icon.png"
+import statusIcon from "../../assets/status-icon.png"
+import notiIcon from "../../assets/notification.png"
+import userIcon from "../../assets/user.png"
+import inReviewIcon from "../../assets/in-review.png";
+import diagnosedIcon from "../../assets/diagnosed.png";
+import totalIcon from "../../assets/total.png";
+import searchRightIcon from "../../assets/search.png";
+
+
+
+
+
+
+import "./HomePage.css";
+
 
 const Homepage = () => {
-  return <div>Homepage</div>;
-};
 
+  const [selectedValue, setSelectedValue] = useState('');
+
+
+  // Function to handle changes in the dropdown selection
+  const handleDropdownChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+  
+  //radio button
+  const [selectedOption, setSelectedOption] = useState('option1');
+  const [selectedOption2, setSelectedOption2] = useState('option1');
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleOptionChange2 = (event) => {
+    setSelectedOption2(event.target.value);
+  };
+
+
+
+
+
+ return(
+  <div className='Homepage'>
+    
+    <div className='top-nav'>
+      <div >
+        <button className='homepage-home-button'>
+            <img src={homePageOneMindLogo} alt="search Logo"/>
+            <p>OneMind</p>
+        </button>
+      </div>
+      <div className='homepage-right-side'>
+       <div className='noti-icon'>
+            <img src={notiIcon} alt="search Logo"/>
+        </div>
+        <div className='noti-icon'>
+            <img src={notiIcon} alt="search Logo"/>
+        </div>
+          <div className='user-icon'>
+            <img src={userIcon} alt="search Logo"/>
+          </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <div className='bottom-content'>
+        <div className='filter-bar'>
+          <div className = 'name-block'>
+            <div className='name-search'>
+                <img src={searchIcon} alt="search Logo"/>
+                  <p>Filter</p> 
+            </div>
+            <input
+                    type="text"
+                    placeholder="ID/Name"
+            /> 
+          </div>
+          <div className = 'age-block'>
+           <div className='age-search'>
+              <img src={ageIcon} alt="search Logo"/>
+                <p>Age</p> 
+            </div>
+              <select id="dropdown" value={selectedValue} onChange={handleDropdownChange}>
+                <option value="">Choose an option</option>
+                <option value="option1">1-10</option>
+                <option value="option2">10-20</option>
+                <option value="option3">30-40</option>
+                <option value="option3">40-50</option>
+                <option value="option3">50-60</option>
+                <option value="option3">60-70</option>
+                <option value="option3">70-80</option>
+
+              </select>
+        </div>
+        <div className = 'date-block'>
+          <div className='date-search'>
+              <img src={dateIcon} alt="search Logo"/>
+                <p>Date</p> 
+          </div>
+          <input
+                    type="text"
+                    placeholder="Select a date"
+                  /> 
+        </div>
+
+
+        <div className = 'gender-block'>
+          <div className='gender-search'>
+              <img src={genderIcon} alt="gender Logo"/>
+                <p>Gender</p> 
+          </div>
+          <label className="gender-radio">
+          Male
+
+        <input
+          type="radio"
+          value="option1"
+          checked={selectedOption === 'option1'}
+          onChange={handleOptionChange}
+        />
+        <span className='checkmark'></span>
+      </label>
+
+      <label className='gender-radio'>
+      Female
+
+        <input
+          value="option2"
+          type="radio"
+          checked={selectedOption === 'option2'}
+          onChange={handleOptionChange}
+        />
+        <span className='checkmark'></span>
+      </label>
+
+      {/* <p>Selected option: {selectedOption}</p> */}
+        </div>
+        <div className = 'name-block'>
+          <div className='name-search'>
+              <img src={phq9Icon} alt="search Logo"/>
+                <p>PHQ-9 Score</p> 
+          </div>
+          <input
+                    type="text"
+                    placeholder="Enter a score"
+                  /> 
+        </div>
+        <div className = 'name-block'>
+          <div className='name-search'>
+              <img src={statusIcon} alt="search Logo"/>
+                <p>Status</p> 
+          </div>
+          <label className="gender-radio">
+          In Review
+
+        <input
+          type="radio"
+          value="option1"
+          checked={selectedOption2 === 'option1'}
+          onChange={handleOptionChange2}
+        />
+        <span className='checkmark'></span>
+      </label>
+
+      <label className='gender-radio'>
+      Diagnosed
+
+        <input
+          value="option2"
+          type="radio"
+          checked={selectedOption2 === 'option2'}
+          onChange={handleOptionChange2}
+        />
+        <span className='checkmark'></span>
+      </label>
+
+      {/* <p>Selected option: {selectedOption}</p> */}
+        </div>
+      </div>
+        
+        <div className='home-main-content'>
+            <div className='diagnosis-details'>
+                <div className='in-review'>
+                  <div className='in-review-header'>
+                    <img src={inReviewIcon} alt="search Logo"/>
+                    <p>In Review</p>
+                  </div>
+                  <div className='in-review-description'>
+                  
+                  <p>  64  </p>
+                  </div>
+                </div>
+                <div className='diagnosed'>
+                <div className='in-review-header'>
+                    <img src={diagnosedIcon} alt="search Logo"/>
+                    <p>Diagnosed</p>
+                  </div>
+                  <div className='in-review-description'>
+                  
+                  <p>  dummy number  </p>
+                  </div>
+                </div>
+                <div className='total'>
+                <div className='in-review-header'>
+                    <img src={totalIcon} alt="search Logo"/>
+                    <p>Total</p>
+                  </div>
+                  <div className='in-review-description'>
+                  
+                  <p>  2,378 </p>
+                  </div>
+                </div>
+            </div>
+            <div className='patient-table'>
+              <div className='patient-table-header'>
+                <p>Patient List</p>
+                <button>+ Add Patient</button>
+              </div>
+
+
+
+
+              <table className='table-field'>
+                <tr>
+                  {[
+                    "Patient Id",
+                    "Name",
+                    "Age",
+                    "Gender",
+                    "Date",
+                    "PHQ9-Score",
+                    "Status",
+                  ].map((thLabel)=>(
+                    <th>{thLabel}</th>
+                  ))}
+                </tr>
+
+                <tr>
+                  {[
+                    "P1929",
+                    "Yong Wing Soon",
+                    "24",
+                    "Male",
+                    "19/2/2010",
+                    "11",
+                    "Diagnosed",
+                  ].map((thLabel)=>(
+                    <td>{thLabel}</td>
+                  ))}
+                </tr>
+
+                <tr>
+                  {[
+                    "P1929",
+                    "Yong Wing Soon",
+                    "24",
+                    "Male",
+                    "19/2/2010",
+                    "11",
+                    "Diagnosed",
+                  ].map((thLabel)=>(
+                    <td>{thLabel}</td>
+                  ))}
+                </tr>
+
+                <tr>
+                  {[
+                    "P1929",
+                    "Yong Wing Soon",
+                    "24",
+                    "Male",
+                    "19/2/2010",
+                    "11",
+                    "Diagnosed",
+                  ].map((thLabel)=>(
+                    <td>{thLabel}</td>
+                  ))}
+                </tr>
+
+                <tr>
+                  {[
+                    "P1929",
+                    "Yong Wing Soon",
+                    "24",
+                    "Male",
+                    "19/2/2010",
+                    "11",
+                    "Diagnosed",
+                  ].map((thLabel)=>(
+                    <td>{thLabel}</td>
+                  ))}
+                </tr>
+
+              </table>
+
+              <div className='home-page-navigation'>
+                <button>Previous</button>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+ ); 
+};
 export default Homepage;
