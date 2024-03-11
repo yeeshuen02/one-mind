6eec70fd7fc95ce6f631665c6869021e479c1571
yeeshuen <yeeshuen02@gmail.com>
2024-03-11 22:00:36 +0800

    Added ScrollToTop to start at top for new page

--- src/App.js
+++ src/App.js
@@ -11,23 +11,90 @@ import Report from "./scenes/Report/Report";
 import Upload from "./scenes/Upload/Upload";
 import { AuthProvider } from "./context/AuthContext";
 import PrivateRoute from "./context/PrivateRoute";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <AuthProvider>
-        <Routes> //basename="/app"
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginContainer />} />
-          <Route path="/homepage" element={<PrivateRoute><Homepage /></PrivateRoute>  } />
-          <Route path="/addpatient" element={<PrivateRoute><AddPatient /></PrivateRoute>} />
-          <Route path="/consent" element={<PrivateRoute><ConsentForm /></PrivateRoute> } />
-          <Route path="/questionnaire" element={<PrivateRoute><Questionnaire /></PrivateRoute> } />
-          <Route path="/results" element={<PrivateRoute><Results /></PrivateRoute> } />
-          <Route path="/results/:patientID" element={<PrivateRoute><Results /></PrivateRoute> } />
-          <Route path="/upload" element={<PrivateRoute><Upload /></PrivateRoute> } />
-          <Route path="/report" element={<PrivateRoute><Report /></PrivateRoute> } />
-          <Route path="/report/:patientID" element={<PrivateRoute><Report /></PrivateRoute> } />
-        </Routes>
+      <ScrollToTop />
+      <Routes>
+        {" "}
+        //basename="/app"
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginContainer />} />
+        <Route
+          path="/homepage"
+          element={
+            <PrivateRoute>
+              <Homepage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/addpatient"
+          element={
+            <PrivateRoute>
+              <AddPatient />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/consent"
+          element={
+            <PrivateRoute>
+              <ConsentForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/questionnaire"
+          element={
+            <PrivateRoute>
+              <Questionnaire />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/results"
+          element={
+            <PrivateRoute>
+              <Results />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/results/:patientID"
+          element={
+            <PrivateRoute>
+              <Results />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/upload"
+          element={
+            <PrivateRoute>
+              <Upload />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/report"
+          element={
+            <PrivateRoute>
+              <Report />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/report/:patientID"
+          element={
+            <PrivateRoute>
+              <Report />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </AuthProvider>
   );
 }
