2002cd42fabb11ac23654e46583568dab7242935
yeeshuen <yeeshuen02@gmail.com>
2024-03-19 18:13:51 +0800

    Update homepage button

--- src/components/Homebutton/Homebutton.jsx
+++ src/components/Homebutton/Homebutton.jsx
@@ -3,27 +3,20 @@ import "./Homebutton.css";
 import homePageOneMindLogo from "../../assets/logo-blue.png";
 import { useNavigate } from "react-router-dom";
 
-
-
-
 const Homebutton = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-
-    const navigateHome = () => {
-       
-            navigate('/homepage')
-        
-      };
-
+  const navigateHome = () => {
+    navigate("/homepage");
+  };
 
   return (
-   <div>
-        <button className="homepage-home-button">
-                <img src={homePageOneMindLogo} onClick={navigateHome} alt="search Logo" />
-                <p>OneMind</p>
-        </button>
-   </div>
+    <div>
+      <button className="homepage-home-button" onClick={navigateHome}>
+        <img src={homePageOneMindLogo} alt="search Logo" />
+        <p>OneMind</p>
+      </button>
+    </div>
   );
 };
 
